abb572f7c499e7ace3090f2b36464767efc83e3e
Benjamin Aplin <benjaminaplin@gmail.com>
2016-01-23 14:08:49 -0500

    set up login

--- app/controllers/used_cars_controller.rb
+++ app/controllers/used_cars_controller.rb
@@ -1,2 +1,13 @@
 class UsedCarsController < ApplicationController
+  before_action :require_user, only: [:index, :show]
+
+  def index
+    @used_cars = UsedCar.all
+  end
+
+  def show
+    @used_car = UsedCar.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'used_cars/:id' => 'used_cars#show', as: :album
   post 'used_cars' => 'used_cars#create'
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   get 'signup'  => 'users#new'
   resources :users
+
 end
